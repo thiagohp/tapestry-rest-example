9290f6200f6dd007699b210c2ced6af598377243
Thiago H. de Paula Figueiredo <thiago@arsmachina.com.br>
2022-01-17 19:30:07 -0300

    Adding custom date format for Jackson's ObjectMapper

--- src/main/java/br/com/machina/tapestryrestexample/services/AppModule.java
+++ src/main/java/br/com/machina/tapestryrestexample/services/AppModule.java
@@ -1,11 +1,16 @@
 package br.com.machina.tapestryrestexample.services;
 
+import java.text.SimpleDateFormat;
+
 import org.apache.tapestry5.SymbolConstants;
 import org.apache.tapestry5.commons.MappedConfiguration;
 import org.apache.tapestry5.commons.OrderedConfiguration;
 import org.apache.tapestry5.http.services.HttpRequestBodyConverter;
 import org.apache.tapestry5.ioc.ServiceBinder;
+import org.apache.tapestry5.jacksondatabind.services.ObjectMapperSource;
 import org.apache.tapestry5.services.ComponentEventResultProcessor;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.machina.tapestryrestexample.rest.entities.User;
 
@@ -21,6 +26,34 @@ public class AppModule {
         configuration.add(SymbolConstants.HMAC_PASSPHRASE, "I need some REST!");
         configuration.add(SymbolConstants.OPENAPI_TITLE, "Tapestry REST support example");
         configuration.add(SymbolConstants.OPENAPI_APPLICATION_VERSION, "0.0.1");
+    }
+    
+    /**
+     * Example of customizing the Jackson Databinding's {@link ObjectMapper}, specifically the
+     * date format.
+     */
+    public static void contributeObjectMapperSource(OrderedConfiguration<ObjectMapperSource> configuration)
+    {
+        configuration.add("Custom", new CustomObjectMapperSource());
+    }
+    
+    private static final class CustomObjectMapperSource implements ObjectMapperSource {
+
+        final private ObjectMapper mapper;
+        
+        public CustomObjectMapperSource() {
+            mapper = new ObjectMapper(/* ... */);
+            mapper.setDateFormat(new SimpleDateFormat("yyyy/MM/dd hh:mm:ss"));
+            // Any other customizations go here.
+        }
+        
+        @Override
+        public ObjectMapper get(Class<?> clasz) {
+            // You could check the class to provide class-specific ObjectMapper 
+            // instances here, but this isn't the case for this example
+            return mapper;
+        }
+        
     }
     
 }
